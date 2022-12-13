03af7a0c29bbeb2f9dadc20bcb0195396fc85f06
primus-android <radul.vasily@yandex.ru>
2022-12-13 07:26:29 +0300

    a

--- dibil_geim.py
+++ dibil_geim.py
@@ -28,17 +28,71 @@ class Player(Entity):
         super().__init__(location)
 
 
+class Wiget():
+    def __init__(self, scr):
+        self.scr = scr
+
+    def draw(self):
+        pass
+
+    def get_event(self):
+        return None
+
+
+class Button(Wiget):
+    def  __init__(self, scr):
+        super().__init__(scr)
+
+
+class Menu():
+    def __init__(self, scr):
+        self.scr = scr
+        self.wigets = []
+
+    def run(self):
+        while True:
+            self.draw()
+            event = self.get_event()
+            if event != None()
+                return event
+
+    def draw(self):
+        for wiget in self.wigets:
+            wiget.draw()
+
+    def get_event(self):
+        for wiget in self.wigets:
+            event = wiget.get_event()
+            if event != None:
+                return event
+        return None:
+
+
+class Game():
+    def __init__(self, scr):
+        self.scr = scr
+
+    def run(self):
+        while True:
+            self.draw()
+
+    def draw(self):
+        pass
+
+
 class App():
     def __init__(self, scr):
         self.scr = scr
         init_pairs()
+        self.menu = Menu(self.scr)
+        self.game = Game(self.scr)
 
     def run(self):
-        while True:
-            pass
-
-    def draw(self):
-        pass
+        while True()
+            if self.menu.run() == 'start':
+                self.game.run()
+            else:
+                break
 
 
 def color_pair_rgb(self, r, g, b):
