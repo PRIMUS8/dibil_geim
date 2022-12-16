ad8103972686deccadea0f106a61ce9d1fb4caa3
primus-android <radul.vasily@yandex.ru>
2022-12-16 18:51:01 +0300

    я почти придумал структуру проекта

--- dibil_geim.py
+++ dibil_geim.py
@@ -13,59 +13,19 @@ class Map():
 
 
 class Entity:
-    def __init__(self, location):
-        self.location = location
+    def __init__(self, pos):
+        self.location = pos
 
-    def move(self, location):
-        self.location = location
+    def move(self, pos):
+        self.pos = pos
 
     def draw(self, scr):
         pass
 
 
 class Player(Entity):
-    def __init__(self, location):
-        super().__init__(location)
-
-
-class Wiget():
-    def __init__(self, scr):
-        self.scr = scr
-
-    def draw(self):
-        pass
-
-    def get_event(self):
-        return None
-
-
-class Button(Wiget):
-    def  __init__(self, scr):
-        super().__init__(scr)
-
-
-class Menu():
-    def __init__(self, scr):
-        self.scr = scr
-        self.wigets = []
-
-    def run(self):
-        while True:
-            self.draw()
-            event = self.get_event()
-            if event != None()
-                return event
-
-    def draw(self):
-        for wiget in self.wigets:
-            wiget.draw()
-
-    def get_event(self):
-        for wiget in self.wigets:
-            event = wiget.get_event()
-            if event != None:
-                return event
-        return None:
+    def __init__(self, pos):
+        super().__init__(pos)
 
 
 class Game():
@@ -80,19 +40,73 @@ class Game():
         pass
 
 
-class App():
-    def __init__(self, scr):
+class Wiget():
+    def __init__(self, scr, pos_proc, dpos_pix, size_pix):
         self.scr = scr
-        init_pairs()
-        self.menu = Menu(self.scr)
-        self.game = Game(self.scr)
+        self.pos_proc = pos_proc
+        self.dpos_pix = dpos_pix
+        self.size_pix = size_pix
+
+    def draw(self):
+        pass
+
+    def get_event(self):
+        return None
+
+
+class Button(Wiget):
+    def  __init__(self, scr, pos_proc, dpos_pix, size_pix):
+        super().__init__(scr, pos_proc, dpos_pix, size_pix)
+
+
+class Page():
+    def __init__(self, scr, wigets, subpages):
+        self.scr = scr
+        self.wigets = wigets
+        self.subpages = subpages
 
     def run(self):
-        while True()
-            if self.menu.run() == 'start':
-                self.game.run()
-            else:
-                break
+        while True:
+            self.draw()
+            self.event_handler()
+
+    def draw(self):
+        for wiget in self.wigets:
+            wiget.draw
+
+    def event_handler(self):
+        pass
+
+
+class Main_menu(Page):
+    def __init__(self, scr):
+        wigets = []
+        subpages = []
+        super().__init__(scr, wigets, subpages)
+
+    def event_handler(self):
+        pass
+
+
+class App():
+    def __init__(self):
+        self.stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(True)
+        curses.mousemask(-1)
+        curses.start_color()
+        init_pairs()
+        self.menu = Main_Menu(self.scr)
+
+    def __del__(self):
+        curses.echo()
+        surses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.endwin()
+
+    def run(self):
+        self.menu.run()
 
 
 def color_pair_rgb(self, r, g, b):
@@ -104,9 +118,9 @@ def init_pairs(self):
 
 
 def main(stdscr):
-    app = App(stdscr)
+    app = App()
     app.run()
 
 
 if __name__ == '__main__':
-    curses.wrapper(main)
+    main()
