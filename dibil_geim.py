1b903311870cd208da04bdbc7ff508eb0ff02163
primus <radul.vasily@yandex.ru>
2022-12-18 22:08:09 +0300

    ну чисто зажол с компа, открыл в пайчарме и убрал всякий брэд который подсвечивался как ошибки

--- dibil_geim.py
+++ dibil_geim.py
@@ -20,7 +20,7 @@ pass
 
 #Виджеты:
 
-class Wiget():
+class Widget:
     def __init__(self, scr, rltv_pos, dpos_pix, size_pix, texture):
         self.scr = scr
         self.rltv_pos = rltv_pos
@@ -29,29 +29,30 @@ class Wiget():
         self.texture = texture
 
     def draw(self):
-        render_texture(self.scr, self.get_pos(), texture[0])
+        render_texture(self.scr, self.get_pos(), self.texture[0])
 
     def get_pos(self):
-        scr_size = scr.getmaxyx()
-        return scr_size[0] * rltv_pos[0] + dpos_pix[0], scr_size[1] * rltv_pos[1] + dpos_pix[1]
+        scr_size = self.scr.getmaxyx()
+        return (int(scr_size[0] * self.rltv_pos[0]) + self.dpos_pix[0],
+                int(scr_size[1] * self.rltv_pos[1]) + self.dpos_pix[1])
 
 
-class FunctionalWiget(Wiget):
-    def __init__(self, scr, rltv_pos, dpos_pix, size_pix texture):
+class FunctionalWidget(Widget):
+    def __init__(self, scr, rltv_pos, dpos_pix, size_pix, texture):
         super().__init__(scr, rltv_pos, dpos_pix, size_pix, texture)
 
-    def handler():
+    def handler(self):
         pass
 
 
-class Button(FunctionalWiget):
-    def __init__(self, scr, rltv_pos, dpos_pix, size_pix):
-        super().__init__(scr, rltv_pos, dpos_pix, size_pix)
+class Button(FunctionalWidget):
+    def __init__(self, scr, rltv_pos, dpos_pix, size_pix, texture):
+        super().__init__(scr, rltv_pos, dpos_pix, size_pix, texture)
 
 
-class TestFuncWiget(FunctionalWiget): #удалить
+class TestFuncWidget(FunctionalWidget): #удалить
     def __init__(self, scr):
-        super().__init__(scr, (0.5, 0.5), (-4, -4), (8, 8))
+        super().__init__(scr, (0.5, 0.5), (-4, -4), (8, 8), load_texture('./ascii_textures/example'))
 
     def draw(self):
         pass
@@ -59,10 +60,10 @@ class TestFuncWiget(FunctionalWiget): #удалить
 
 #Страницы меню:
 
-class Page():
-    def __init__(self, scr, wigets, pasepages, subpages):
+class Page:
+    def __init__(self, scr, widgets, basepages, subpages):
         self.scr = scr
-        self.wigets = wigets
+        self.widgets = widgets
         self.subpages = subpages
         self.basepages = basepages
 
@@ -72,77 +73,77 @@ class Page():
             self.handler()
 
     def draw(self):
-        for wiget in self.wigets:
-            wiget.draw
+        for widget in self.widgets:
+            widget.draw()
 
     def handler(self):
-        for wiget in wigets:
-            if isinstance(wiget, FunctuonalWiget):
-                wiget.handler()
+        for widget in self.widgets:
+            if isinstance(widget, FunctionalWidget):
+                widget.handler()
 
 
-class Game_config_menu(Page):
-    def __init__(self, scr, root, back):
-        wigets = []
+class GameConfigMenu(Page):
+    def __init__(self, scr, basepages):
+        widgets = []
         subpages = []
-        super().__init__(scr, wigets, [root, back], subpages)
+        super().__init__(scr, widgets, basepages, subpages)
 
 
-class Settings_menu(Page):
-    def __init__(self, scr, root, back):
-        wigets = []
+class SettingsMenu(Page):
+    def __init__(self, scr, basepages):
+        widgets = []
         subpages = []
-        super().__init__(scr, wigets, [root, back], subpages)
+        super().__init__(scr, widgets, basepages, subpages)
 
 
-class Main_menu(Page):
+class MainMenu(Page):
     def __init__(self, scr):
-        wigets = []
-        subpages = [Game_config_menu(scr, self, self),
-                    Settings_menu(scr, self, self)]
-        super().__init__(scr, wigets, subpages)
+        widgets = []
+        subpages = [GameConfigMenu(scr, [self, self]),
+                    SettingsMenu(scr, [self, self])]
+        super().__init__(scr, widgets, [self, self], subpages)
 
 
 #ПРИЛОЖЕНИЕ:
 
-class App():
+class App:
     def __init__(self):
-        self.stdscr = curses.initscr()
+        self.scr = curses.initscr()
         curses.noecho()
         curses.cbreak()
-        self.stdscr.keypad(True)
+        self.scr.keypad(True)
         curses.mousemask(-1)
         curses.start_color()
         curses.curs_set(False)
         init_pairs()
-        self.menu = Main_Menu(self.scr)
+        self.menu = MainMenu(self.scr)
 
     def __del__(self):
         curses.echo()
-        surses.nocbreak()
-        self.stdscr.keypad(False)
+        curses.nocbreak()
+        self.scr.keypad(False)
         curses.endwin()
 
     def run(self):
         self.menu.run()
 
 
-#ДОП. ФУНКЦИИ:
+#ФУНКЦИИ:
 
 #Цвета:
 
-def color_pair_rgb(self, r, g, b):
+def color_pair_rgb(r, g, b):
     return curses.color_pair(16 + 36*r + 6*g + b)
 
 
-def init_pairs(self):
+def init_pairs():
     for c in range(256):
         if c != 0: curses.init_pair(c, c, 0)
 
 
 #Текстуры:
 
-def unpack_texture(path):
+def load_texture(path):
     texture = open(path, 'r').read()
     texture = texture.split('\n')
     shape = [int(i) for i in texture.pop(0).split('x')]
@@ -162,7 +163,7 @@ def render_texture(scr, pos, texture):
 
 #ЗАПУСК:
 
-def main(stdscr):
+def main():
     app = App()
     app.run()
 
